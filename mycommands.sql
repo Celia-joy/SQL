e0939501ae63a0c25e11d0943a493a03df8fa909
Celia <joyihirwecelia@gmail.com>
2026-03-23 21:50:38 +0200

    Code base

---
+++ mycommands.sql
@@ -0,0 +1,30 @@
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| bookstore          |
+| company            |
+| hospitaldb         |
+| information_schema |
+| library            |
+| mysql              |
+| performance_schema |
+| revision           |
+| school             |
+| student_db         |
+| university         |
+| universitydb       |
++--------------------+
+12 rows in set (0.00 sec)
+
+mysql> USE universitydb;
+Database changed
+mysql>  SHOW TABLES;
++------------------------+
+| Tables_in_universitydb |
++------------------------+
+| grades                 |
++------------------------+
+1 row in set (0.00 sec)
+
+mysql> Terminal close -- exit!
